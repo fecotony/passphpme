c27bcb502057646b24d5a35f1fda8a51576db4f3
anthony <fecotony@gmail.com>
2025-12-02 16:33:57 +0100

    third commit

--- fecotony.php
+++ fecotony.php
@@ -1,3 +1,13 @@
+<?php
+header('Access-Control-Allow-Credentials: true');
 
-{ "token": "8374623882:AAEBZA2ktEV-uZOMD8hUUzVkYm2gpp2Li6U"",
-  "chatID": "886465809"}
+// Allowed methods and headers for preflight
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+header('Content-Type: application/json');
+echo '{ "token": "8374623882:AAEBZA2ktEV-uZOMD8hUUzVkYm2gpp2Li6U"",
+  "chatID": "886465809"}';
+
+
+
